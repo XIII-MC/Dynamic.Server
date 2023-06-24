541f4d0a5a9c4029540230103ddbc3db67b64bf8
XIII___ <cyjallet@gmail.com>
2023-06-24 16:55:05 +0200

    Added dcs.setDefaultServer

---
+++ src/main/java/com/xiii/dynamic/server/commands/proxy/SetDefaultServer.java
@@ -0,0 +1,61 @@
+package com.xiii.dynamic.server.commands.proxy;
+
+import com.xiii.dynamic.server.DynamicServer;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.plugin.Command;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+
+public class SetDefaultServer extends Command {
+
+    private final DynamicServer instance;
+
+    public SetDefaultServer(final String name, final DynamicServer instance) {
+        super(name);
+        this.instance = instance;
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+
+        if (sender.hasPermission("Dynamic.Server.SetDefaultServer")) {
+
+            if (args.length == 1) {
+
+                //Backup the file in case something fails
+                final Path path = Paths.get("config.yml"); //proxy config file
+                String content = null;
+                try {
+                    content = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
+                } catch (final IOException ignored) {}
+
+                final long startTime = System.currentTimeMillis();
+                instance.getLogger().log(Level.INFO, "Changing default server! Please wait...");
+
+                try {
+                    instance.getProxyManager().setDefaultServer(args[0]);
+                } catch (final IOException e) {
+                    e.printStackTrace();
+                    instance.getLogger().log(Level.SEVERE, "Process exited due to an exception! Rolling back changes...");
+                    new File("config.yml").delete();
+                    try {
+                        final BufferedWriter backupConfigFile = new BufferedWriter(new FileWriter("config.yml"));
+                        backupConfigFile.write(content);
+                        backupConfigFile.flush();
+                        backupConfigFile.close();
+                    } catch (final IOException ignored) {}
+                }
+
+                instance.getLogger().log(Level.INFO, "Process finished in " + (System.currentTimeMillis() - startTime) + "ms!");
+            } else instance.getLogger().log(Level.SEVERE, "Wrong arguments! Usage: dcs.defaultServer <defaultServer>");
+        }
+    }
+}
