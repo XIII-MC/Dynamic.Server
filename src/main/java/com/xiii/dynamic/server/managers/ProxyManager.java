b30b369b702a4619a6711ceba5c853760def637a
XIII___ <cyjallet@gmail.com>
2023-06-24 16:54:29 +0200

    Fixed host port change, made all parts into methods

--- src/main/java/com/xiii/dynamic/server/managers/ProxyManager.java
+++ src/main/java/com/xiii/dynamic/server/managers/ProxyManager.java
@@ -2,9 +2,6 @@ package com.xiii.dynamic.server.managers;
 
 import com.xiii.dynamic.server.DynamicServer;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.net.ConnectException;
 import java.net.Socket;
@@ -19,7 +16,9 @@ public class ProxyManager {
 
     private final DynamicServer instance;
 
-    final Charset charset = StandardCharsets.UTF_8;
+    private final Charset charset = StandardCharsets.UTF_8;
+
+    private final Path configYml = Paths.get("config.yml");
 
     public ProxyManager(final DynamicServer instance) {
         this.instance = instance;
@@ -27,69 +26,82 @@ public class ProxyManager {
 
     public void configureProxy(final String defaultServer, final String ipForward, final String onlineMode, final String startPort) throws IOException {
 
-        instance.getLogger().log(Level.INFO, "Updating proxy's configuration! Please wait...");
+        //Remove 'forced' BungeeCord settings
+        clearForcedSettings();
 
-        //Pre cache config file
-        final Path path = Paths.get("config.yml"); //proxy config file
-        String content = new String(Files.readAllBytes(Paths.get("config.yml")), charset);
-        Files.write(path, content.getBytes());
-        String updatedContent;
+        setDefaultServer(defaultServer);
 
+        if (ipForward != null) toggleIPForward(Boolean.parseBoolean(ipForward));
+
+        if (onlineMode != null) toggleOnlineMode(Boolean.parseBoolean(onlineMode));
+
+        if (startPort != null) changeProxyPort(Integer.parseInt(startPort));
+
+        instance.getLogger().log(Level.WARNING, "Server configured! You will have to restart your proxy to apply changes.");
+    }
+
+    public void clearForcedSettings() throws IOException {
+        final String content = new String(Files.readAllBytes(configYml), charset);
+        final String updatedContent = content.replace("md_5", "§").replace("- admin", "- §").replace("lobby", "§").replace("localhost:25565", "0:0").replace("restricted: false", "restricted: true").replace("&1Just another BungeeCord - Forced Host", "").replace("pvp.md-5.net: pvp", "§: §").replace("&1Another Bungee server", "A Minecraft Server");
+        Files.write(configYml, updatedContent.getBytes(charset));
+        instance.getLogger().log(Level.INFO, "Forced settings removed");
+    }
+
+    public void setDefaultServer(final String defaultServer) throws IOException {
+        final String content = new String(Files.readAllBytes(configYml), charset);
+        final String updatedContent = content.replaceAll("priorities:\\n.*", "priorities:" + System.lineSeparator() + "  - " + defaultServer);
         if (!content.contains(defaultServer)) {
-            instance.getLogger().log(Level.WARNING, defaultServer + " doesn't appear in the linked servers list! It was set anyway.");
+            instance.getLogger().log(Level.SEVERE, defaultServer + " doesn't appear in the linked servers list! Canceling...");
+            return;
         }
-        content = new String(Files.readAllBytes(Paths.get("config.yml")), charset);
-        updatedContent = new StringBuilder(content).insert(content.indexOf("priorities:"), "priorities:" + System.lineSeparator() + "  - " + defaultServer).toString().replace(defaultServer + "priorities:", defaultServer);
-        Files.write(path, updatedContent.getBytes(charset));
+        Files.write(configYml, updatedContent.getBytes(charset));
         instance.getLogger().log(Level.INFO, "Set default server to " + defaultServer);
+    }
 
-        if (ipForward != null) {
-            content = new String(Files.readAllBytes(Paths.get("config.yml")), charset);
-            updatedContent = content.replaceAll("ip_forward: " + !Boolean.parseBoolean(ipForward), "ip_forward: " + Boolean.parseBoolean(ipForward));
-            Files.write(path, updatedContent.getBytes(charset));
-            instance.getLogger().log(Level.INFO, "Set IP Forwarding to " + Boolean.parseBoolean(ipForward));
-        }
+    public void toggleIPForward(final boolean bool) throws IOException {
+        final String content = new String(Files.readAllBytes(configYml), charset);
+        final String updatedContent = content.replaceAll("ip_forward: " + !bool, "ip_forward: " + bool);
+        Files.write(configYml, updatedContent.getBytes(charset));
+        instance.getLogger().log(Level.INFO, "Set IP Forwarding to " + bool);
+    }
 
-        if (onlineMode != null) {
-            content = new String(Files.readAllBytes(Paths.get("config.yml")), charset);
-            updatedContent = content.replaceAll("online_mode: " + !Boolean.parseBoolean(onlineMode), "online_mode: " + Boolean.parseBoolean(onlineMode));
-            Files.write(path, updatedContent.getBytes(charset));
-            instance.getLogger().log(Level.INFO, "Set online mode to " + Boolean.parseBoolean(onlineMode));
-        }
+    public void toggleOnlineMode(final boolean bool) throws IOException {
+        final String content = new String(Files.readAllBytes(configYml), charset);
+        final String updatedContent = content.replaceAll("online_mode: " + !bool, "online_mode: " + bool);
+        Files.write(configYml, updatedContent.getBytes(charset));
+        instance.getLogger().log(Level.INFO, "Set online mode to " + bool);
+    }
 
-        if (startPort != null) {
-            instance.getLogger().log(Level.INFO, "Searching for available ports starting from " + startPort);
+    public void changeProxyPort(final int startPort) throws IOException {
+        instance.getLogger().log(Level.INFO, "Searching for available ports starting from " + startPort);
 
-            //Find an available port
-            boolean availablePort = false;
-            int portNumber = Integer.parseInt(startPort);
-            for (int port = Integer.parseInt(startPort); !availablePort; port++) {
-                try (Socket ignored = new Socket("localhost", port)) {
-                } catch (final ConnectException e) {
-                    if (!content.contains("localhost" + ":" + port)) {
-                        availablePort = true;
-                        portNumber = port;
-                    }
+        String content = new String(Files.readAllBytes(configYml), charset);
+
+        //Find an available port
+        boolean availablePort = false;
+        int portNumber = startPort;
+        for (int port = startPort; !availablePort; port++) {
+            try (Socket ignored = new Socket("localhost", port)) {
+            } catch (final ConnectException e) {
+                if (!content.contains("localhost" + ":" + port)) {
+                    availablePort = true;
+                    portNumber = port;
                 }
             }
-
-            if (!availablePort) {
-                instance.getLogger().log(Level.WARNING, "Couldn't find any free port! The port was not changed.");
-            }
-
-            if (portNumber != Integer.parseInt(startPort)) {
-                instance.getLogger().log(Level.WARNING, "The new port is not desired " + startPort + " port! Port did not change.");
-            } else {
-                content = new String(Files.readAllBytes(Paths.get("config.yml")), charset);
-                updatedContent = content.replaceAll("query_port:.*", "query_port: " + portNumber);
-                Files.write(path, updatedContent.getBytes(charset));
-                //content = new String(Files.readAllBytes(Paths.get("config.yml")), charset);
-                //updatedContent = content.replaceAll("/\\w+$/", String.valueOf(portNumber));
-                //Files.write(path, updatedContent.getBytes(charset));
-                //TODO: fix
-                instance.getLogger().log(Level.INFO, "Query and host ports set to " + portNumber);
-            }
         }
-        instance.getLogger().log(Level.WARNING, "Server configured! You will have to restart your proxy to apply changes.");
+
+        if (!availablePort) {
+            instance.getLogger().log(Level.WARNING, "Couldn't find any free port! The port was not changed.");
+            return;
+        }
+
+        content = new String(Files.readAllBytes(configYml), charset);
+        String updatedContent = content.replaceAll("query_port:.*", "query_port: " + portNumber);
+        Files.write(configYml, updatedContent.getBytes(charset));
+        content = new String(Files.readAllBytes(configYml), charset);
+
+        updatedContent = content.replaceAll("((?<=host: ).*)(?<=:).*", "0.0.0.0:" + portNumber); ///(?<=host: ).*(?<=:)/g | Save IP
+        Files.write(configYml, updatedContent.getBytes(charset));
+        instance.getLogger().log(Level.INFO, "Query and host ports set to " + portNumber);
     }
 }
