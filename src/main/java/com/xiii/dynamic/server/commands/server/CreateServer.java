34300131e99300f31bed5444bef947c108575b38
XIII___ <cyjallet@gmail.com>
2023-06-25 16:23:05 +0200

    Added better exceptions and potential error handling

--- src/main/java/com/xiii/dynamic/server/commands/server/CreateServer.java
+++ src/main/java/com/xiii/dynamic/server/commands/server/CreateServer.java
@@ -1,11 +1,14 @@
 package com.xiii.dynamic.server.commands.server;
 
 import com.xiii.dynamic.server.DynamicServer;
+import com.xiii.dynamic.server.utils.FileUtils;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.plugin.Command;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.logging.Level;
 
 public class CreateServer extends Command {
@@ -26,22 +29,34 @@ public class CreateServer extends Command {
 
             if (args.length >= 5) {
 
-                final long startTime = System.currentTimeMillis();
-                if (args.length >= 7) tempStartPort = Integer.parseInt(args[6]);
-                if (args.length >= 6) tempDistantIP = args[5];
+                if (args[4].equalsIgnoreCase("false") || args[4].equalsIgnoreCase("true")) {
 
-                try {
-                    instance.getServerManager().createServer(Integer.parseInt(args[0]), args[1], args[2], args[3], Boolean.parseBoolean(args[4]), tempDistantIP, tempStartPort);
-                } catch (final IOException e) {
-                    e.printStackTrace();
-                    instance.getLogger().log(Level.SEVERE, "Process exited due to an exception! Canceling server creation...");
-                    new File(args[1]).delete();
-                }
-                instance.getLogger().log(Level.INFO, "Process finished in " + (System.currentTimeMillis() - startTime) + "ms!");
+                    try {
+                        if (args.length >= 7) tempStartPort = Integer.parseInt(args[6]);
+                    } catch (final NumberFormatException e) {
+                        instance.getLogger().log(Level.SEVERE, "Wrong arguments! Usage: dcs.createServer <Xmx> <Name> <Server_software> <Server_version> <auto_config> (distant_ip) !>>>(start_port)<<<!");
+                    }
+                    if (args.length >= 6) tempDistantIP = args[5];
 
+                    final long startTime = System.currentTimeMillis();
+
+                    try {
+                        instance.getServerManager().createServer(Integer.parseInt(args[0]), args[1], args[2], args[3], Boolean.parseBoolean(args[4]), tempDistantIP, tempStartPort);
+                    } catch (final NumberFormatException e) {
+                        instance.getLogger().log(Level.SEVERE, "Wrong arguments! Usage: dcs.createServer !>>><Xmx><<<! <Name> <Server_software> <Server_version> <auto_config> (distant_ip) (start_port)");
+                    } catch (final IOException e) {
+                        e.printStackTrace();
+                        instance.getLogger().log(Level.SEVERE, "Process exited due to an exception! Canceling server creation...");
+
+                        try {
+                            FileUtils.deleteDirectory(new File(args[1]));
+                        } catch (final IOException ex) {}
+                    }
+                    instance.getLogger().log(Level.INFO, "Process finished in " + (System.currentTimeMillis() - startTime) + "ms!");
+                } else instance.getLogger().log(Level.SEVERE, "Wrong arguments! Usage: dcs.createServer <Xmx> <Name> <Server_software> <Server_version> !>>><auto_config><<<! (distant_ip) (start_port)");
             } else instance.getLogger().log(Level.SEVERE, "Wrong arguments! Usage: dcs.createServer <Xmx> <Name> <Server_software> <Server_version> <auto_config> (distant_ip) (start_port)");
+            tempStartPort = -1;
+            tempDistantIP = null;
         }
-        tempStartPort = -1;
-        tempDistantIP = null;
     }
 }
