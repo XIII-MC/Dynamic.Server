2f81930d617b3ff4c8b27946d9f787aedac9483e
XIII___ <cyjallet@gmail.com>
2023-06-29 14:28:53 +0200

    Test

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,22 +1,25 @@
-
 import java.io.IOException;
-import java.net.ConnectException;
-import java.net.Socket;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
-    private static final String ipForward = "true";
 
     public static void main(String[] args) throws IOException {
         final Path path = Paths.get("D:\\Bureau\\Dynamic.Server\\config.yml"); //proxy config file
         final String content = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
 
-        final String subString = content.substring(content.indexOf("servers:"));
-        final boolean matches = subString.contains("VanTes:");
-        System.out.println(subString + " " + matches);
+        final String servers = content.substring(content.indexOf("servers:"));
+        final Pattern r = Pattern.compile("(?<=address: ).*");
+        final Matcher m = r.matcher(servers);
+
+        while (m.find()) {
+            final String port = m.group().substring(m.group().indexOf(":")).replace(":", "");
+            final String ip = m.group().substring(0, m.group().indexOf(":"));
+            System.out.println("ip=" + ip + " port=" + port);
+        }
     }
 }
